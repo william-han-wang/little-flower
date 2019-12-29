493ff9de85c67b69d57049aaaed43412b73d64d1
william-han-wang <uniwanghan@gmail.com>
2019-12-28 19:14:55 -0500

    Update my site

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 enable_jsdelivr = {
-    "enabled": True,
+    "enabled": False,
     "repo": "william-han-wang/LittleFlower@gh-pages"
 }
 
